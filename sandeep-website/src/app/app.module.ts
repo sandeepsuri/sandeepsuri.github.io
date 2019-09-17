cfa35e8d2575f47ae8c5aa4652a42b3a55ff8b11
Sandeep Suri <sandeep.suri@ryerson.ca>
2019-09-17 00:45:47 -0400

    navbar 1.0 working
    just a basic navbar created using bootstrap, got the route for it working (home about and contact) right now "snake" is not working (future games tab for users to play around with)

--- sandeep-website/src/app/app.module.ts
+++ sandeep-website/src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Router
+import {RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './elements/navbar/navbar.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+//Routes
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
